a80c9a55b432c54f1e835ce3f7246c27c3c64387
arthu <arthur.badretinov@mail.ru>
2021-06-28 19:53:58 +0500

    Было сделано приложение, которое открывает окно с единственной кнопкой, нажав на которую открывается еще одно окно, в котором выбираем файлы, и после берутся данные с этих файлов

--- PyQT_3D-Visualisierung/main.py
+++ PyQT_3D-Visualisierung/main.py
@@ -3,30 +3,41 @@ import numpy as np
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QFileDialog, QApplication, QMainWindow
 
-def file_reading(files):
-    data = {}
-    for file in files:
-        f = open(file, mode='r')
-        array_list = []
-        for linenum, line in enumerate(f.readlines()):
-            if linenum == 1:
-                name = line.split()[3]
-            if linenum >= 12:
-                x = float(line.split()[1])
-                y = float(line.split()[2])
-                z = float(line.split()[3])
-                array = np.array([x, y, z])
-                array_list.append(array)
-        array_array = np.array(array_list)
-        data[name] = array_array
-    return data
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(100, 50, 1280, 960)
+
+        btn = QtWidgets.QPushButton(self)
+        btn.move(10,10)
+        btn.setText("Выбрать файлы")
+        btn.adjustSize()
+        btn.clicked.connect(self.get_file_names)
+
+    def get_file_names(self):
+        files = QFileDialog.getOpenFileNames(None, "Выберите файлы:", "", "Text files (*.dev)")
+        print(self.file_reading(files[0]))
+
+    def file_reading(self, files):
+        data = {}
+        for file in files:
+            f = open(file, mode='r')
+            array_list = []
+            for linenum, line in enumerate(f.readlines()):
+                if linenum == 1:
+                    name = line.split()[3]
+                elif linenum >= 12:
+                    x = float(line.split()[1])
+                    y = float(line.split()[2])
+                    z = float(line.split()[3])
+                    array = np.array([x, y, z])
+                    array_list.append(array)
+            array_array = np.array(array_list)
+            data[name] = array_array
+        return data
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = QMainWindow
-    window.setGeometry(300, 250, 350, 200)
+    window = Window()
     window.show()
-
-    #files = QFileDialog.getOpenFileNames(None, "Выберите файлы:", "", "Text files (*.dev)")
-    #print(file_reading(files[0]))
     sys.exit(app.exec_())
