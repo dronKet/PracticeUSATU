dd26a48684d3f4ad835ae7861b0718fed2c95248
rdmrk <kar.radmir@gmail.com>
2021-07-01 00:52:19 +0500

    Web: Модели скважин и координат

--- Web/WellProject/models.py
+++ Web/WellProject/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 
 # Create your models here.
-class Well(models.Model):
+class WellModel(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 
-class Coordinate(models.Model):
-    well = models.ForeignKey(Well, on_delete=models.CASCADE)
-    data = models.JSONField()
-
-
+class CoordinateModel(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
+    well = models.ForeignKey(WellModel, on_delete=models.CASCADE)
+    sequence_number = models.IntegerField()
+    x = models.FloatField()
+    y = models.FloatField()
+    z = models.FloatField()
--- Web/WellProject/views.py
+++ Web/WellProject/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 
-from .mocks import Well
+from .models import WellModel, CoordinateModel
 
 
 # Create your views here.
 def index(request):
-    wells = Well.all()
+    wells = WellModel.objects.all()
     return render(request, 'WellProject/index.html', {'wells': wells})
 
 
 def show(request, id):
-    well = Well.find(id)
-    mylist = zip(well['Coordinate']['x'], well['Coordinate']['y'], well['Coordinate']['z'])
-    return render(request, 'WellProject/show.html', {'well': well, 'coords': mylist})
+    well = WellModel.objects.get(id=id)
+    coords = CoordinateModel.objects.filter(well__id=id)
+    return render(request, 'WellProject/show.html', {'well': well, 'coords': coords})
